f0c06cc550c4310a397ee092c5ad578e74bc1e9b
Rachel <rachel.ulgado@gmail.com>
2018-10-09 14:40:02 -0700

    adding link to list of voices

--- hello-node/03-child-process.js
+++ hello-node/03-child-process.js
@@ -1,8 +1,10 @@
 // Credit to Mark Hellar for this example 😎
 const exec = require('child_process').exec;
 
+// a list of voices can be found here:
+// https://gist.github.com/mculp/4b95752e25c456d425c6
 function say(msg, name) {
-  exec('say ' + msg + ' ' + name);
+  exec('say -v Sam ' + msg + ' ' + name);
 }
 
 say('Hello nice to meet you', 'Rachel');
