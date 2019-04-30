cb538122a531c4accf05266f6b52872136dcd91e
rose-water <rachel.ulgado@gmail.com>
2019-04-29 19:07:57 -0700

    updates with api stuff

--- web-server/index.js
+++ web-server/index.js
@@ -6,19 +6,38 @@ const PORT    = 3000;
 // tell our app where to serve our static files
 app.use(express.static('public'));
 
+// -----------------------------------------------------
 // define a route - what happens when people visit /
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
+// -----------------------------------------------------
+// define another route - this just prints out the request params
+// enter in your browser: http://localhost:3000/test?species=cats
+app.get('/test', function(req, res) {
+  let cats = ["Scottish Fold", "Russian Blue", "Ragdoll"];
+  let dogs = ["Golden Retriever", "German Shepherd", "Alaskan Malamute"];
+  
+  if (req.query.species == 'cats') {
+    res.send(cats);
+  } else if (req.query.species == 'dogs') {
+    res.send(dogs);
+  } else {
+    res.send("Unknown Species");
+  }
+});
+
+// -----------------------------------------------------
 // example API request - more on this on thursday
 let url = 'https://api.carbonintensity.org.uk/intensity/factors';
 let options = {
-  json: true
+  json: true 
 };
 
 request(url, options, function(err, res, body) {
   if (!err && res.statusCode == 200) {
+    // Uncomment the following to log the response data
     console.log(body.data);
   } else {
     console.log(err);
@@ -29,4 +48,3 @@ request(url, options, function(err, res, body) {
 app.listen(PORT, function() {
   console.log('listening on PORT: ' + PORT);
 });
-
